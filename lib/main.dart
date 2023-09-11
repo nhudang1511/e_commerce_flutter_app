5c721ec44cac28be9d85ec64f37fd7585a98b0bf
nhu_nguyen <nhudang1511@gmail.com>
2023-09-11 12:39:53 +0700

    add firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:e_commerce/blocs/cart/cart_event.dart';
 import 'package:e_commerce/blocs/wishlist/wishlist_event.dart';
 import 'package:e_commerce/config/app_route.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'blocs/cart/cart_bloc.dart';
@@ -8,7 +9,9 @@ import 'blocs/wishlist/wishlist_bloc.dart';
 import 'config/theme.dart';
 import 'screen/screen.dart';
 
-void main() {
+Future <void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
