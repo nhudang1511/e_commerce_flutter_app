7d1e8639d2245659c17b45a58c4381c93d20fda9
nhu_nguyen <nhudang1511@gmail.com>
2023-09-13 09:56:08 +0700

    update catalog_screen with firebase

--- lib/screen/catalog/catalog_screen.dart
+++ lib/screen/catalog/catalog_screen.dart
@@ -1,42 +1,62 @@
-import 'package:e_commerce/models/category_model.dart';
 import 'package:e_commerce/models/models.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../blocs/product/product_bloc.dart';
+import '../../blocs/product/product_state.dart';
 import '../../widget/widgets.dart';
 
 class CatalogScreen extends StatelessWidget {
   static const String routeName = '/catalog';
 
-  static Route route({required Category category}){
+  static Route route({required Category category}) {
     return MaterialPageRoute(
         settings: const RouteSettings(name: routeName),
         builder: (_) => CatalogScreen(category: category));
   }
 
   final Category category;
+
   const CatalogScreen({super.key, required this.category});
+
   @override
   Widget build(BuildContext context) {
-    final List<Product> categoryProducts = Product.products.where((element) => element.category == category.name).toList();
+    //final List<Product> categoryProducts = Product.products.where((element) => element.category == category.name).toList();
     return Scaffold(
       appBar: CustomAppBar(title: category.name),
       bottomNavigationBar: const CustomeNavBar(),
-      body: GridView.builder(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 8.0,
-          vertical: 16.0
-        ),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2, childAspectRatio: 1.15),
-        itemBuilder: (BuildContext context, int index){
-          return Center(
-              child: ProductCard(
-                product: categoryProducts[index],
-                widthFactor: 2.2,
+      body: BlocBuilder<ProductBloc, ProductState>(
+        builder: (context, state) {
+          if(state is ProductLoading){
+            return const Center(child: CircularProgressIndicator(),);
+          }
+          if(state is ProductLoaded){
+            final List<Product> categoryProducts = state.products.where((element) {
+              return element.category == category.name;
+            }).toList();
+
+            return GridView.builder(
+              padding: const EdgeInsets.symmetric(
+                  horizontal: 8.0,
+                  vertical: 16.0
               ),
-          );
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, childAspectRatio: 1.15),
+              itemBuilder: (BuildContext context, int index) {
+                return Center(
+                  child: ProductCard(
+                    product: categoryProducts[index],
+                    widthFactor: 2.2,
+                  ),
+                );
+              },
+              itemCount: categoryProducts.length,
+            );
+          }
+          else{
+            return const Text('Something went wrong');
+          }
         },
-        itemCount: categoryProducts.length,
       ),
     );
   }
